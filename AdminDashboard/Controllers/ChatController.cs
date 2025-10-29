b642873005dd4bece05c894c6b10541a84558644
HaiDang-Ng22 <dh813345@gmail.com>
2025-10-29 20:10:21 +0700

    thêm Phần tin  nhắn và fix lỗi Tài Xế

---
+++ AdminDashboard/Controllers/ChatController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AdminDashboard.Controllers
+{
+    [Authorize]
+    public class ChatController : Controller
+    {
+        public IActionResult Index(string receiverId = "")
+        {
+            ViewBag.ReceiverId = receiverId;
+            return View();
+        }
+    }
+}
