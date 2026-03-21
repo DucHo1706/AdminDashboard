7929094fcabdf8fd622d14f6762a96f23d82a359
EinnUlfr01 <dieptunguyen1@gmail.com>
2026-03-21 10:48:27 +0700

    Adapter Pattern

---
+++ AdminDashboard/Models/PaymentResult.cs
@@ -0,0 +1,11 @@
+﻿namespace AdminDashboard.Models
+{
+    public class PaymentResult
+    {
+        public bool Success { get; set; }
+        public string GatewayName { get; set; }
+        public string PaymentUrl { get; set; }
+        public string TransactionCode { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ AdminDashboard/Patterns/Adapter/IPaymentGateway.cs
@@ -0,0 +1,9 @@
+﻿using AdminDashboard.Models;
+
+namespace AdminDashboard.Patterns.Adapter
+{
+    public interface IPaymentGateway
+    {
+        PaymentResult ProcessPayment(PaymentRequest request);
+    }
+}
